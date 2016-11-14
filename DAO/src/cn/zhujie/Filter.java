15839c06a0ff4b35df95f1a8585eae070cad8167
wei <wei@DESKTOP-LNBGLN8>
2016-11-14 21:54:56 +0800

    WEI

---
+++ DAO/src/cn/zhujie/Filter.java
@@ -0,0 +1,59 @@
+package cn.zhujie;
+
+public class Filter {
+
+	private int id;
+	private String userName;
+	private String nickName;
+	private int age;
+	private String city;
+	
+	
+	public Filter() {
+		
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getNickName() {
+		return nickName;
+	}
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	private String email;
+	private String mobile;
+}
